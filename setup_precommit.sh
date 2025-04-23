71812b894d48f5a84341649fa2e7cbbb16e370c6
dsomni <pro100pro10010@gmail.com>
2025-04-23 23:46:46 +0300

    Improve [report] report

--- setup_precommit.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-echo "#!/bin/sh
-uv sync
-uv run ruff format .
-uv run ruff check . --fix --exit-non-zero-on-fix
-FILES=\$(git diff --diff-filter=d --name-only --staged)
-git add \$FILES
-" > .git/hooks/pre-commit
-chmod +x .git/hooks/pre-commit
